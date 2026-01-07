7eeab5b9af46ec95d9f702000148711e4197abe5
Ahmed Hamza Rana <ahmedhamzarana000@gmail.com>
2026-01-07 18:38:35 +0500

    Resolve bugs
    Removed comments for CSS file sections in register.php.

--- Admin/register.php
+++ Admin/register.php
@@ -8,14 +8,11 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Register &mdash; CodiePie</title>
 
-    <!-- General CSS Files -->
     <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
 
-    <!-- CSS Libraries -->
     <link rel="stylesheet" href="assets/modules/jquery-selectric/selectric.css">
 
-    <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style.min.css">
     <link rel="stylesheet" href="assets/css/components.min.css">
 </head>
